8e66e0b38c2cf0e58ee91bc1e1cdb724054b349e
Michael Gatto <mgatto@vinsuite.com>
2026-03-09 09:27:06 -0700

    feat: add artifacts to be stolen

---
+++ include/carmen/artifact.h
@@ -0,0 +1,17 @@
+#ifndef CARMEN_ARTIFACT_H
+#define CARMEN_ARTIFACT_H
+
+#include "carmen/carmen_export.h"
+
+#define CARMEN_ARTIFACT_COUNT 18
+
+typedef struct {
+    const char *id;
+    const char *name;            /* i18n key */
+    const char *origin_city_id;  /* city the artifact was stolen from */
+    const char *description;     /* i18n key */
+} CarmenArtifact;
+
+CARMEN_API extern const CarmenArtifact CARMEN_ARTIFACTS[CARMEN_ARTIFACT_COUNT];
+
+#endif
---
+++ src/artifact.c
@@ -0,0 +1,93 @@
+#include "carmen/artifact.h"
+
+const CarmenArtifact CARMEN_ARTIFACTS[CARMEN_ARTIFACT_COUNT] = {
+    {"astrolabe_isfahan",
+     "artifact.astrolabe_isfahan.name",
+     "isfahan",
+     "artifact.astrolabe_isfahan.desc"},
+
+    {"quran_manuscript_cairo",
+     "artifact.quran_manuscript_cairo.name",
+     "cairo",
+     "artifact.quran_manuscript_cairo.desc"},
+
+    {"iznik_tile_istanbul",
+     "artifact.iznik_tile_istanbul.name",
+     "istanbul",
+     "artifact.iznik_tile_istanbul.desc"},
+
+    {"khanjar_muscat",
+     "artifact.khanjar_muscat.name",
+     "muscat",
+     "artifact.khanjar_muscat.desc"},
+
+    {"brass_lamp_cairo",
+     "artifact.brass_lamp_cairo.name",
+     "cairo",
+     "artifact.brass_lamp_cairo.desc"},
+
+    {"whirling_robe_konya",
+     "artifact.whirling_robe_konya.name",
+     "konya",
+     "artifact.whirling_robe_konya.desc"},
+
+    {"samarkand_paper_tashkent",
+     "artifact.samarkand_paper_tashkent.name",
+     "tashkent",
+     "artifact.samarkand_paper_tashkent.desc"},
+
+    {"mughal_miniature_lahore",
+     "artifact.mughal_miniature_lahore.name",
+     "lahore",
+     "artifact.mughal_miniature_lahore.desc"},
+
+    {"zellige_mosaic_fez",
+     "artifact.zellige_mosaic_fez.name",
+     "fez",
+     "artifact.zellige_mosaic_fez.desc"},
+
+    {"dead_sea_scrolls_amman",
+     "artifact.dead_sea_scrolls_amman.name",
+     "amman",
+     "artifact.dead_sea_scrolls_amman.desc"},
+
+    {"pearl_chest_dubai",
+     "artifact.pearl_chest_dubai.name",
+     "dubai",
+     "artifact.pearl_chest_dubai.desc"},
+
+    {"vakufnama_sarajevo",
+     "artifact.vakufnama_sarajevo.name",
+     "sarajevo",
+     "artifact.vakufnama_sarajevo.desc"},
+
+    {"zanzibar_door",
+     "artifact.zanzibar_door.name",
+     "zanzibar",
+     "artifact.zanzibar_door.desc"},
+
+    {"muslin_dhaka",
+     "artifact.muslin_dhaka.name",
+     "dhaka",
+     "artifact.muslin_dhaka.desc"},
+
+    {"pewter_tankard_kuala_lumpur",
+     "artifact.pewter_tankard_kuala_lumpur.name",
+     "kuala_lumpur",
+     "artifact.pewter_tankard_kuala_lumpur.desc"},
+
+    {"keris_jakarta",
+     "artifact.keris_jakarta.name",
+     "jakarta",
+     "artifact.keris_jakarta.desc"},
+
+    {"cedar_oud_beirut",
+     "artifact.cedar_oud_beirut.name",
+     "beirut",
+     "artifact.cedar_oud_beirut.desc"},
+
+    {"glass_weight_dakar",
+     "artifact.glass_weight_dakar.name",
+     "dakar",
+     "artifact.glass_weight_dakar.desc"},
+};
