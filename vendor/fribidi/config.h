f0c5519874e32ce0514eb228c5d52853a6703ced
Michael Gatto <mgatto@vinsuite.com>
2026-08-28 21:11:14 -0700

    fix: compile error on Windows
    Should match the Git tag.

--- vendor/fribidi/config.h
+++ vendor/fribidi/config.h
@@ -5,7 +5,10 @@
 #define HAVE_STRINGIZE 1
 #define HAVE_STDLIB_H 1
 #define HAVE_STRING_H 1
-#define HAVE_STRINGS_H 1
 #define STDC_HEADERS 1
+/* POSIX strings.h (strcasecmp, bzero, …) is not present on MSVC. */
+#if !defined(_MSC_VER)
+#define HAVE_STRINGS_H 1
+#endif
 
 #endif
